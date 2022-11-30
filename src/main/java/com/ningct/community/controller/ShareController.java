4c629d540bc806f7d6ea1cf0049d204f8d5275da
ningct <2640069987@qq.com>
2022-11-30 21:46:24 +0800

    1

--- src/main/java/com/ningct/community/controller/ShareController.java
+++ src/main/java/com/ningct/community/controller/ShareController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -25,21 +26,24 @@ public class ShareController implements CommunityConstant {
 
     @RequestMapping(path = "/share",method = RequestMethod.GET)
     @ResponseBody
-    public String share(String htmlUrl){
-        //生成文件名
-        String fileName = CommunityUtil.generateUUID();
-        //异步生成长图
-        Event even = new Event();
-        even.setTopic(TOPIC_SHARE);
-        even.setData("fileName",fileName)
-                .setData("htmlUrl",htmlUrl)
-                .setData("suffix",".png");
-        eventProducer.fireEvent(even);
-        //返回图片访问路径：阿里云路径
-        Map<String, Object> map = new HashMap<>();
-        String host = "https://" +bucketName+ "." +endPoint;
-        map.put("shareUrl",host+"/"+fileName +".png");
-
-        return CommunityUtil.getJSONString(0,null,map);
+    public String share(@RequestParam(name = "type") int type, @RequestParam(name = "id") int id){
+        System.out.println(type);
+        System.out.println(id);
+//        //生成文件名
+//        String fileName = CommunityUtil.generateUUID();
+//        //异步生成长图
+//        Event even = new Event();
+//        even.setTopic(TOPIC_SHARE);
+//        even.setData("fileName",fileName)
+//                .setData("htmlUrl",htmlUrl)
+//                .setData("suffix",".png");
+//        eventProducer.fireEvent(even);
+//        //返回图片访问路径：阿里云路径
+//        Map<String, Object> map = new HashMap<>();
+//        String host = "https://" +bucketName+ "." +endPoint;
+//        map.put("shareUrl",host+"/"+fileName +".png");
+//
+//        return CommunityUtil.getJSONString(0,null,map);
+        return "yes";
     }
 }
